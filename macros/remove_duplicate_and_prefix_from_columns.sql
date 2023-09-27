e065401de4d59566cb43554a719ef1735f354049
Greg Finley <greg@mozartdata.com>
2023-09-27 12:02:41 -0700

    Fix dedupe logic

--- macros/remove_duplicate_and_prefix_from_columns.sql
+++ macros/remove_duplicate_and_prefix_from_columns.sql
@@ -1,12 +1,16 @@
 {% macro remove_duplicate_and_prefix_from_columns(columns, prefix='', exclude=[]) %}
-        
+        {# Loop once to find duplicates #}
         {%- set duplicate_exclude = [] -%}
         {% for col in columns if col.name not in exclude %}
-        {%- for dupe in columns if col.name[prefix|length:]|lower == dupe.name -%}
+        {%- for dupe in columns if col.name[prefix|length:]|lower == dupe.name|lower -%}
         {%- do duplicate_exclude.append(col.name) -%}
         {%- do duplicate_exclude.append(dupe.name) -%}
         , coalesce({{ col.name }}, {{dupe.name}}) as {{ col.name[prefix|length:] }}
         {%- endfor %}
+        {% endfor %}
+        
+        {# Loop again to find non-duplicates #}
+        {% for col in columns if col.name not in exclude %}
         {%- if col.name not in duplicate_exclude -%}
         {% if col.name[:prefix|length]|lower == prefix %}
         , {{ col.name }} as {{ col.name[prefix|length:] }}
